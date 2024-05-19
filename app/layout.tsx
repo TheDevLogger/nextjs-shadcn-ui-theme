6f3a6ebbdd5a62839bf076fb6c55025a2139bc02
DevLogger <thedevloger@gmail.com>
2024-05-18 20:41:40 -0700

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
+import ThemeDataProvider from "@/context/theme-data-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <NextThemesProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ThemeDataProvider>{children}</ThemeDataProvider>
+        </NextThemesProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,42 @@
+import { ThemeColorToggle } from "@/components/theme-color-toggle";
+import { ThemeModeToggle } from "@/components/theme-mode-toggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p
+          className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300
+            bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl
+            dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static
+            lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 text-primary"
+        >
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div
+          className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t
+            from-white via-white dark:from-black dark:via-black lg:static lg:size-auto
+            lg:bg-none gap-x-1"
+        >
+          <ThemeColorToggle />
+          <ThemeModeToggle />
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      <div
+        className="relative z-[-1] flex place-items-center before:absolute before:h-[300px]
+          before:w-full before:-translate-x-1/2 before:rounded-full
+          before:bg-gradient-radial before:from-white before:to-transparent
+          before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px]
+          after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200
+          after:via-blue-200 after:blur-2xl after:content-['']
+          before:dark:bg-gradient-to-br before:dark:from-transparent
+          before:dark:to-primary before:dark:opacity-10 after:dark:from-sky-900
+          after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px]
+          sm:after:w-[240px] before:lg:h-[360px]"
+      >
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
@@ -39,16 +47,24 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <div
+        className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4
+          lg:text-left gap-x-2"
+      >
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border bg-card px-5 py-4 transition-colors
+            hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700
+            hover:dark:bg-neutral-800/30 dark:border-white dark:border-opacity-5"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
+          <h2 className="mb-3 text-2xl font-semibold text-primary">
             Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            <span
+              className="inline-block transition-transform group-hover:translate-x-1
+                motion-reduce:transform-none"
+            >
               -&gt;
             </span>
           </h2>
@@ -59,13 +75,18 @@ export default function Home() {
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border bg-card px-5 py-4 transition-colors
+            hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700
+            hover:dark:bg-neutral-800/30 dark:border-white dark:border-opacity-5"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
+          <h2 className="mb-3 text-2xl font-semibold text-primary">
             Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            <span
+              className="inline-block transition-transform group-hover:translate-x-1
+                motion-reduce:transform-none"
+            >
               -&gt;
             </span>
           </h2>
@@ -76,13 +97,18 @@ export default function Home() {
 
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border bg-card px-5 py-4 transition-colors
+            hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700
+            hover:dark:bg-neutral-800/30 dark:border-white dark:border-opacity-5"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
+          <h2 className="mb-3 text-2xl font-semibold text-primary">
             Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            <span
+              className="inline-block transition-transform group-hover:translate-x-1
+                motion-reduce:transform-none"
+            >
               -&gt;
             </span>
           </h2>
@@ -93,13 +119,18 @@ export default function Home() {
 
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border bg-card px-5 py-4 transition-colors
+            hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700
+            hover:dark:bg-neutral-800/30 dark:border-white dark:border-opacity-5"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
+          <h2 className="mb-3 text-2xl font-semibold text-primary">
             Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            <span
+              className="inline-block transition-transform group-hover:translate-x-1
+                motion-reduce:transform-none"
+            >
               -&gt;
             </span>
           </h2>
